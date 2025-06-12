7010238fdba4bc843d400b2b2c557ce32181ba9a
ragnarune <ragnarunevps@gmail.com>
2025-06-11 18:18:59 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-apt-get update -y && apt-get install -y tmate && \
-tmate
+# Start tmate in background
+tmate -S /tmp/tmate.sock new-session -d
+tmate -S /tmp/tmate.sock wait tmate-ready
+tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
+
+# Start fake web server on port 10000 (any unused port)
+while true; do echo -e "HTTP/1.1 200 OK\n\nWorking" | nc -l -p 10000 -q 1; done
